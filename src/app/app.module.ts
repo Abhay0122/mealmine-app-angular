f081aa1891b5d9ea77e4266208fd9e6bd809ec82
Abhay parmar <117059180+Abhay0122@users.noreply.github.com>
2023-04-30 17:42:16 +0530

    first commit
    mealmine app

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FoodFormComponent } from './food-form/food-form.component';
+import { FoodItemComponent } from './food-item/food-item.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FoodFormComponent,
+    FoodItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
